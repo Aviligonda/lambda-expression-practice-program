9ee0c8754ff3addb2b9392ebbe5a5f1a5e0a68d5
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-05 13:02:17 +0530

    Added : iterating through from differnt method to print the values

---
+++ NumberPlayList.java
@@ -0,0 +1,45 @@
+package com.bridzelab;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i);
+        }
+        // simple foreach implicit lambda function
+        list.forEach(
+                numbers -> System.out.println("Lambda Implicit ForEach  is :" + numbers)
+        );
+        // consumer class
+        class MyConsumer implements Consumer<Integer> {
+            public void accept(Integer i) {
+                System.out.println("Consumer Class  is :" + i);
+            }
+        }
+        MyConsumer action = new MyConsumer();
+        list.forEach(action);
+        // through iterating
+        Iterator<Integer> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            Integer i = iterator.next();
+            System.out.println("Through Iterator  is :" + i);
+        }
+        /// using anonymous class
+        list.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer i) {
+                System.out.println("ForEach anonymous class is :" + i);
+            }
+        });
+        // explicit lambda function
+        Consumer<Integer> listAction = n -> {
+            System.out.println("Lambda Foreach explicit is :" + n);
+        };
+        list.forEach(listAction);
+    }
+}
